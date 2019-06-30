a7e071b203daf48bf1d8bdd0febe2cd714985662
shingo tokuyasu <singo.2525@gmail.com>
2019-06-30 12:50:05 +0900

    修正しました

--- js/main.js
+++ js/main.js
@@ -1,90 +1,86 @@
 (function(){
   'use strict';
-    var startBtn = document.getElementById('start');
-    var genre = document.getElementById('genre');
-    var diff = document.getElementById('difficulty');
-    var qs = document.getElementById('qs');
-    var ques = document.getElementById('question');
-    var qsNum = document.getElementById('qs-num');
-    var ansOne = document.getElementById('ans-one');
-    var ansSec = document.getElementById('ans-sec');
-    var ansThir = document.getElementById('ans-thir');
-    var ansFour = document.getElementById('ans-four');
-    var btn = document.getElementsByClassName('btn');
-    var back = document.getElementById('back');
-    var score = 0;
-    var count = 0;
-    var answs = [];
+    const startBtn = document.getElementById('start');
+    const genre = document.getElementById('genre');
+    const diff = document.getElementById('difficulty');
+    const qs = document.getElementById('qs');
+    const answs = document.getElementById('answers');
+    const qsNum = document.getElementById('qs-num');
+    const btn = document.getElementsByClassName('btn');
+    const genreArea = document.getElementById('genreArea');
+    let score = 0;
+    let count = 0;
+    let answsArr = [];
+
+    //API
     fetch('https://opentdb.com/api.php?amount=10')
     .then(response =>{
       return response.json();
     })
     .then(data =>{
-      // オブジェクトをシャッフル
-      var objectKeysShuffled = function(dataresults){
-        return shuffle(Object.keys(data.results));
-      }
-        setQuiz();
-     //クイズをセットする処理
-     function setQuiz(){
-       var qsCount = count+1;
-       console.log(data.results[count]);//テストアウトプット
-       genre.innerText ="[ジャンル] "+data.results[count].category;
-       genre.style.display="block";
-       diff.innerText ="[難易度] "+data.results[count].difficulty;
-       diff.style.display="block";
-       qs.innerText = data.results[count].question;
-       qsNum.innerText="問題"+qsCount;
-       buildAnsChoises();
-         //回答選択肢押下時処理
-       for(var i=0; i<btn.length;i++){
-           btn[i].addEventListener('click',function(){
-           if(this.value===data.results[count].correct_answer){
-             score++;
-             count++;
-           }else{
-             count++;
-           }
-           if(count===10){
-             qsNum.innerHTML="あなたの正解数は"+score+"です！";
-             qs.innerHTML="再度チャレンジする場合は以下をクリック";
-             genre.style.display="none";
-             diff.style.display = "none";
-           }
-           answs=[];
-           ques.textContent = [];
-           setQuiz();
-         });
-       }
-     }
-     //  回答選択肢作成処理
-     function buildAnsChoises(){
-         // correct_answerを配列answsへプッシュ
-       answs.push(data.results[count].correct_answer);
-       //incorrect_answersを配列answsへプッシュ
-       for(var i=0;i<data.results[count].incorrect_answers.length;i++){
-         answs.push(data.results[count].incorrect_answers[i])
-       }
-       //answsをシャッフル
-       var length = answs.length;
-       for(var i = length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var tmp = answs[i];answs
-        answs[i] = answs[j];
-        answs[j] = tmp;
-       }
-       //DOM生成処理
-       for(var i =0;i<answs.length;i++){
-         var dd = document.createElement('dd');
-         var inp = document.createElement('input');
-         ques.appendChild(dd);
-         dd.appendChild(inp);
-         inp.type="button";
-         inp.className="btn";
-         inp.value = answs[i];
-       }
-     }
+      setQuiz(data);
     });
-})();
 
-// プルリクエスト
+     //クイズをセットする処理
+     function setQuiz(data){
+       let qsCount = count + 1;
+       genre.innerText = "[ジャンル] " + data.results[count].category;
+       genre.style.display = "block";
+       diff.innerText = "[難易度] " + data.results[count].difficulty;
+       diff.style.display = "block";
+       qs.innerText = data.results[count].question;
+       qsNum.innerText ="問題" + qsCount;
+
+       buildAnsChoises(data);
+
+     }
+
+     // 回答選択肢の作成
+     function buildAnsChoises(data){
+        // correct_answerを配列answsArrへプッシュ
+       answsArr.push(data.results[count].correct_answer);
+       // incorrect_answersを配列answsArrへプッシュ
+       for(let i = 0; i < data.results[count].incorrect_answers.length; i ++){
+         answsArr.push(data.results[count].incorrect_answers[i])
+       }
+       // answsArrをシャッフル
+       let length = answsArr.length;
+       for(let i = length - 1; i > 0; i --) {
+         let j = Math.floor(Math.random() * (i + 1));
+         let tmp = answsArr[i];
+         answsArr[i] = answsArr[j];
+         answsArr[j] = tmp;
+       }
+
+       // DOM生成処理
+       for(let i = 0; i < answsArr.length; i ++){
+         let dd = document.createElement('dd');
+         let inp = document.createElement('input');
+         answs.appendChild(dd);
+         dd.appendChild(inp);
+         inp.type = "button";
+         inp.className = "btn";
+         inp.value = answsArr[i];
+       }
+
+       // btn押下時処理
+       const btns = Array.from(btn);
+       btns.forEach(function(el){
+         el.addEventListener('click',function(){
+           count ++;
+           if(this.value === data.results[count].correct_answer){
+             score ++;
+           }
+           // １０問目が終了した時の表示
+           if(count === 10){
+              qsNum.innerHTML = "あなたの正解数は" + score + "です！";
+              qs.innerHTML = "再度チャレンジする場合は以下をクリック";
+              genreArea.textContent = "";
+           }
+           answsArr = [];
+           answs.textContent = [];
+           setQuiz(data);
+        });
+      });
+     }
+})();
